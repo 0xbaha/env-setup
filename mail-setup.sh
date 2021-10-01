92b6e55da7a2b260696d543df17aecd2f0dbd2e8
ba1x <12247434+ba1x@users.noreply.github.com>
2021-10-01 14:05:54 +0700

    update mail-setup.sh

--- mail-setup.sh
+++ mail-setup.sh
@@ -2,7 +2,8 @@
 
 
 FILE_TEMP="temp.txt"
-FILE_CONFIG_POSTFIX="/etc/postfix/main.cf"
+FILE_CONFIG_POSTFIX_MAIN="/etc/postfix/main.cf"
+FILE_CONFIG_POSTFIX_MASTER="/etc/postfix/master.cf"
 FILE_CONFIG_ALIASES="/etc/aliases"
 
 
@@ -29,18 +30,18 @@ TEMP_PRINT="Replace server hostname with email hostname in Postfix config"
 printf "$TEMP_PRINT\n"
 
 # Delete lines that contain a pattern of 'myhostname =' and 'mydestination ='
-sudo sed -i '/myhostname =/d' $FILE_CONFIG_POSTFIX
-sudo sed -i '/mydestination =/d' $FILE_CONFIG_POSTFIX
+sudo sed -i '/myhostname =/d' $FILE_CONFIG_POSTFIX_MAIN
+sudo sed -i '/mydestination =/d' $FILE_CONFIG_POSTFIX_MAIN
 
 # Add new line with new values of 'myhostname' and 'mydestination'
-printf "myhostname = ${EMAIL_HOSTNAME}\n" | sudo tee -a $FILE_CONFIG_POSTFIX
-printf "mydestination = \$myhostname, ${EMAIL_DOMAIN}, ${EMAIL_HOSTNAME}, localhost.${EMAIL_DOMAIN}, localhost\n" | sudo tee -a $FILE_CONFIG_POSTFIX
+printf "myhostname = ${EMAIL_HOSTNAME}\n" | sudo tee -a $FILE_CONFIG_POSTFIX_MAIN
+printf "mydestination = \$myhostname, ${EMAIL_DOMAIN}, ${EMAIL_HOSTNAME}, localhost.${EMAIL_DOMAIN}, localhost\n" | sudo tee -a $FILE_CONFIG_POSTFIX_MAIN
 
 sudo systemctl reload postfix
 
 # Check Postfix config after edit
-cat $FILE_CONFIG_POSTFIX | grep myhostname
-cat $FILE_CONFIG_POSTFIX | grep mydestination
+cat $FILE_CONFIG_POSTFIX_MAIN | grep myhostname
+cat $FILE_CONFIG_POSTFIX_MAIN | grep mydestination
 
 # Add new line with new values of aliases
 printf "root:\t${EMAIL_USER_ALIAS}\n" | sudo tee -a $FILE_CONFIG_ALIASES
@@ -69,6 +70,27 @@ TEMP_PRINT="Open TCP Port 25 (inbound) in Firewall"
 printf "$TEMP_PRINT\n"
 
 sudo ufw allow 25/tcp 
+
+
+# Configuring SPF Policy Agen to detecting forged incoming emails.
+sudo apt install postfix-policyd-spf-python -y
+
+# Edit the Postfix master process configuration file to start the SPF policy daemon when it’s starting itself.
+TEMP_PRINT_1="policyd-spf  unix  -       n       n       -       0       spawn"
+TEMP_PRINT_2="user=policyd-spf argv=/usr/bin/policyd-spf"
+printf "\n$TEMP_PRINT_1\n  $TEMP_PRINT_2\n" | sudo tee -a $FILE_CONFIG_POSTFIX_MASTER
+
+# Impose a restriction on incoming emails by rejecting unauthorized email and checking SPF record.
+TEMP_PRINT_1="policyd-spf_time_limit = 3600"
+TEMP_PRINT_2="smtpd_recipient_restrictions ="
+TEMP_PRINT_3="permit_mynetworks,"
+TEMP_PRINT_4="permit_sasl_authenticated,"
+TEMP_PRINT_5="reject_unauth_destination,"
+TEMP_PRINT_6="check_policy_service unix:private/policyd-spf"
+printf "\n$TEMP_PRINT_1\n$TEMP_PRINT_2\n   $TEMP_PRINT_3\n   $TEMP_PRINT_4\n   $TEMP_PRINT_5\n   $TEMP_PRINT_6\n" | sudo tee -a $FILE_CONFIG_POSTFIX_MAIN
+sudo systemctl restart postfix
+
+# -------------------------------------------------------------------
 
 # Sending Test Email
 send_test_email() {
