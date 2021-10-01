87d002c2750aa094623d1e4969203bc87f7da644
ba1x <12247434+ba1x@users.noreply.github.com>
2021-10-01 07:05:47 +0700

    init mail-setup.sh

---
+++ mail-setup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# update package list
+sudo apt update
+
+# install postfix
+sudo apt install postfix -y
+
+# install the telnet utility to check if it’s open or blocked.
+sudo apt install telnet -y
+
+# Check Postfix version with this command:
+postconf mail_version
+
+# The ss (Socket Statistics) utility tells us that the Postfix master process is listening on TCP port 25.
+sudo ss -lnpt | grep master 
+
+# Postfix ships with many binaries under the /usr/sbin/ directory, as can be seen with the following command.
+dpkg -L postfix | grep /usr/sbin/ 
+
+# Open TCP Port 25 (inbound) in Firewall
+sudo ufw allow 25/tcp 
+
+# Sending Test Email
+send_test_email() {
+
+    TEMP_PRINT="Postfix sending test email"
+    printf "$TEMP_PRINT\n"
+    read -p "Receiver (eg, user@mail.com) []: " EMAIL_FOR_TEST_POSTFIX
+    read -p "Message []: " MESSAGE_FOR_TEST_POSTFIX
+    echo "$MESSAGE_FOR_TEST_POSTFIX" | sendmail $EMAIL_FOR_TEST_POSTFIX
+
+}
+
+# Run the following command on your mail server.
+echo -e '\x1dquit\x0d' | sleep 3 | telnet gmail-smtp-in.l.google.com 25 > temp.txt
+CHECK_PORT_25=$(cat test.txt | grep "220 mx.google.com")
+if [ "$CHECK_PORT_25" != "" ]; then 
+    echo "Port 25... OK"
+    send_test_email
+else 
+    echo "ERROR: Port 25 CLOSED!"
+fi
+rm temp.txt
+
+
+# REFERENCE
+# https://stackoverflow.com/a/54364978
